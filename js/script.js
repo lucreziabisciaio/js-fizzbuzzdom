ef36d10bffded7e4c6e0fdde0d5b010214bd2c1e
lucreziabisciaio <lucreziabisciaio@gmail.com>
2021-12-10 16:54:50 +0100

    writed instruction

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const boxContainer = document.querySelector('.box-container');
+
+// se il contenitore % 3 = 0 e !=0 allora è multiplo di 3 > aggiungo box-3
+// se il contenitore % 5 = 0 e !=0 allora è multiplo di 5 > aggiungo box-5
+// ALTRIMENTI se il contenitore % 3 && % 5 = 0 e !=0 allora è multiplo di entrambi > aggiungo box-ex
+
