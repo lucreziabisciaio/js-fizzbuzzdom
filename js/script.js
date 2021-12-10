d0a0fd1f374bd8790e2be509fd7102e625c8fd7c
lucreziabisciaio <lucreziabisciaio@gmail.com>
2021-12-10 17:10:35 +0100

    wrote instruction and function (multiple of 3 and 5)

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ const boxContainer = document.querySelector('.box-container');
 // se il contenitore % 5 = 0 e !=0 allora è multiplo di 5 > aggiungo box-5
 // ALTRIMENTI se il contenitore % 3 && % 5 = 0 e !=0 allora è multiplo di entrambi > aggiungo box-ex
 
+for (let i = 0; i < 100; i++) {
+    if (i % 3 === 0 && i !== 0) {
+        // divisibile per 3
+        boxContainer.innerHTML += `<div class="box box-3">fizz</div>`  
+    }
+    else if (i % 5 === 0 && i !== 0) {
+        // divisibile per 5
+        boxContainer.innerHTML += `<div class="box box-5">buzz</div>` 
+    } else {
+        boxContainer.innerHTML += `<div class="box">${i}</div>`
+    }
+
+}
